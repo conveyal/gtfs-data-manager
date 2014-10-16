d62821618f4facd566d4e8533f6b18da0735baa9
Matthew Wigginton Conway <matt@indicatrix.org>
2014-10-16 10:08:13 -0400

    show date as a date.

---
+++ public/client/helpers/dateRender.js
@@ -0,0 +1,46 @@
+// a helper to render a date in milliseconds since epoch as something a normal
+// human can understand
+
+// get the month in the local language
+var getMonth = function (month) {
+    return [
+        Messages('app.date.january'),
+        Messages('app.date.february'),
+        Messages('app.date.march'),
+        Messages('app.date.april'),
+        Messages('app.date.may'),
+        Messages('app.date.june'),
+        Messages('app.date.july'),
+        Messages('app.date.august'),
+        Messages('app.date.september'),
+        Messages('app.date.october'),
+        Messages('app.date.november'),
+        Messages('app.date.december')
+    ][month];
+};
+        
+
+/**
+ * Parameters: the date in milliseconds since epoch
+ * whether to include the time, or just the date (default: include time)
+ */
+module.exports = [
+    'renderDate',
+    function (date, includeTime) {
+        if (typeof(includeTime) == 'undefined' || includeTime == null) {
+            var includeTime = true;
+        }
+
+        var d = new Date(date);
+        
+        // TODO: time zone?
+        if (includeTime) {
+            return d.getDate() + ' ' + getMonth(d.getMonth()) + ' ' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ' ';
+        }
+        else {
+            return d.getDate() + ' ' + getMonth(d.getMonth()) + ' ' + d.getFullYear();
+        }
+    }
+];
+            
+            
