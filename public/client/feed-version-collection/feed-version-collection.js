8e6ba5187e59078006c432753090249b025c6b1c
Matthew Wigginton Conway <matt@indicatrix.org>
2014-10-30 09:38:36 -0400

    redefine comparator to sort first by feed source name.

--- public/client/feed-version-collection/feed-version-collection.js
+++ public/client/feed-version-collection/feed-version-collection.js
@@ -5,8 +5,14 @@ var FeedVersion = require('feed-version');
 module.exports = Backbone.Collection.extend({
     model: FeedVersion,
     url: 'api/feedversions',
-    // reverse-sort by version
-    comparator: function (model) {
-        return -model.get('version');
+    // sort by name, then reverse-sort by version
+    comparator: function (model0, model1) {
+        if (model0.get('feedSource').name < model1.get('feedSource').name)
+          return -1;
+        else if (model0.get('feedSource').name > model1.get('feedSource').name)
+          return 1;
+
+        // names are the same, sort by version
+        return model1.get('version') - model0.get('version');
     }
 });
