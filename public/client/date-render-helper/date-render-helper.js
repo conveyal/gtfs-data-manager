331f5fda8e8e0fb5b2202c0d48085610286dce03
Matthew Wigginton Conway <matt@indicatrix.org>
2014-11-04 16:01:54 -0500

    fix #25.

--- public/client/date-render-helper/date-render-helper.js
+++ public/client/date-render-helper/date-render-helper.js
@@ -2,25 +2,7 @@
 // human can understand
 
 var Handlebars = require('handlebars');
-
-// get the month in the local language
-var getMonth = function (month) {
-    return [
-        window.Messages('app.date.january'),
-        window.Messages('app.date.february'),
-        window.Messages('app.date.march'),
-        window.Messages('app.date.april'),
-        window.Messages('app.date.may'),
-        window.Messages('app.date.june'),
-        window.Messages('app.date.july'),
-        window.Messages('app.date.august'),
-        window.Messages('app.date.september'),
-        window.Messages('app.date.october'),
-        window.Messages('app.date.november'),
-        window.Messages('app.date.december')
-    ][month];
-};
-        
+var moment = require('moment');
 
 /**
  * Parameters: the date in milliseconds since epoch
@@ -32,21 +14,7 @@ Handlebars.registerHelper(
         if (date === 0 || date === null)
             return '-';
 
-        if (typeof(includeTime) == 'undefined' || includeTime === null) {
-            includeTime = true;
-        }
-
-        var d = new Date(date);
-        
         // TODO: time zone?
-        if (includeTime) {
-            return d.getDate() + ' ' + getMonth(d.getMonth()) + ' ' + d.getFullYear() + ' ' + d.getHours() + ':' + 
-                (d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes());
-        }
-        else {
-            return d.getDate() + ' ' + getMonth(d.getMonth()) + ' ' + d.getFullYear();
-        }
+        return moment(date).fromNow();
     }
 );
-            
-            
