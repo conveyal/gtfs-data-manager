681da2183e1792341b2dfe73d2044ff52691e6d0
Matthew Wigginton Conway <matt@indicatrix.org>
2014-11-17 15:48:17 -0500

    also remove bounds from deployments (snuck in there by mistake).

--- migrations/addEditorId.js
+++ migrations/addEditorId.js
@@ -15,4 +15,12 @@ dump.feedSources.forEach(function (source) {
   }
 });
 
+// bounds should not be included in deployments, since they are calculated on the fly
+// but there was a bug before this migration was created that caused them to be, and then the files would fail
+// reserialization. So we drop the field
+dump.deployments.forEach(function (d) {
+  if (d.bounds !== undefined)
+    delete d.bounds;
+});
+
 fs.writeFileSync(process.argv[process.argv.length - 1], JSON.stringify(dump));
