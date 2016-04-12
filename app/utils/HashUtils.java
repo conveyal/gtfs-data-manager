a0955938eeb28d8a85a1e53e8629f097aacb0128
David Emory <demory@conveyal.com>
2016-04-11 20:21:43 -0400

    Use CRC32 as hash

--- app/utils/HashUtils.java
+++ app/utils/HashUtils.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -31,41 +32,7 @@ public class HashUtils {
 		
 		try {
 			
-			MessageDigest md = MessageDigest.getInstance("MD5");
-			
-			FileInputStream fis = new FileInputStream(file);
-			
-			DigestInputStream dis = new DigestInputStream(fis, md);
-			
-			
-			// hash the size
-			dis.read(ByteBuffer.allocate(8).putLong(file.length()).array());
-			
-			
-			// hash first 1000 bytes
-			int i = 0;
-			while (dis.read() != -1 && i < 1000) {
-				i++;
-			};
-			
-			// hash  5000 bytes starting in the middle or the remainder of the file if under 10000
-			if(file.length() > 10000) {
-				dis.skip(file.length() / 2);
-				
-				i = 0;
-				while (dis.read() != -1 && i < 5000) {
-					i++;
-				};
-			}
-			else {
-				while (dis.read() != -1) {
-				};
-			}
-				
-			dis.close();
-			
-			return new String(Hex.encodeHex(md.digest()));
-			
+			return "" + FileUtils.checksumCRC32(file);
 			
 		}
 		catch(Exception e) {
@@ -75,3 +42,4 @@ public class HashUtils {
 	}
 
 }
+
